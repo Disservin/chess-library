720504e53b9422728532534926dcdf34ef800830
disservin <45608332+Disservin@users.noreply.github.com>
2022-04-17 15:46:40 +0200

    legal move generation

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,52 @@
 
 using namespace Chess;
 
-int main() {
-    Board board = Board(trickyPosition);
-    board.print();
+Board board = Board();
 
-    Moves moveList = board.generatePseudoLegalMoves();
-    for (int i = 0; i < moveList.size(); i++) {
-        printMove(moveList[i]);
+unsigned long long perft(int depth, int maxDepth) {
+    if (depth == 0) {
+        return 1;
     }
-    std::cout << "Number of moves: " << moveList.size() << std::endl;
+
+    unsigned long long count = 0;
+    unsigned long long total = 0;
+    Moves moveList = board.generateLegalMoves();
+    for (int i = 0; i < (int)moveList.size(); i++) {
+        Move move = moveList[i];
+        board.makemove(move);
+        count += perft(depth - 1, maxDepth);
+        board.unmakemove();
+        if (depth == maxDepth){
+            std::cout << squareToString[move.source()] << squareToString[move.target()];
+            std::cout << ": " << count << std::endl;
+            total += count;
+            count = 0;
+        }
+    }
+    if (depth == maxDepth)
+        std::cout << "Total: " << total << std::endl;
+    return count;
+}
+
+int main() {
+    // 
+    while (true){
+        std::string input;
+        std::getline(std::cin, input);
+        if (input.find("position fen") != std::string::npos) {
+            std::size_t start_index = input.find("fen");
+			std::string fen = input.substr(start_index + 4);
+            board.parseFEN(fen);
+        }
+        if (input.find("go perft") != std::string::npos){
+            std::size_t start_index = input.find("perft");
+			std::string depth_str = input.substr(start_index + 6);
+			int depth = std::stoi(depth_str);
+            perft(depth, depth);
+        }
+        if (input == "quit")
+            break;
+        if (input == "print")
+            board.print();
+    }
 }
