d3a1732e435aa590388f91b0324ed7aea84790de
disservin <45608332+Disservin@users.noreply.github.com>
2022-07-17 23:18:09 +0200

    new main

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,9 @@
 
 using namespace Chess;
 
-Board board = Board();
 uint64_t nodes;
 
-uint64_t perft(int depth) {
+uint64_t perft(Board& board, int depth) {
     Moves moveList = board.legal_moves();
     if (depth == 1) {
         return (int)moveList.count;
@@ -14,20 +13,19 @@ uint64_t perft(int depth) {
     for (int i = 0; i < (int)moveList.count; i++) {
         Move move = moveList.moves[i];
         board.make_move(move);
-        nodes += perft(depth - 1);
+        nodes += perft(board, depth - 1);
         board.unmake_move(move);
     }
     return nodes;
 }
 
 int main() {
-    
-    board.parseFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    Board board = Board();
     auto t1 = std::chrono::high_resolution_clock::now();
-    uint64_t n = perft(6);
+    uint64_t n = perft(board, 6);
     auto t2 = std::chrono::high_resolution_clock::now();
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
     
-    std::cout << "nodes: " << n << " nps " << (n * 1000) / ms << std::endl;
+    std::cout << "nodes " << n << " nps " << (n * 1000) / ms << std::endl;
     return 0;
 }
