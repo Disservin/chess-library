8a487cb8404931baf710d0085632769761e99152
disservin <45608332+Disservin@users.noreply.github.com>
2023-05-01 11:21:25 +0200

    parallel perft

--- src/main.cpp
+++ src/main.cpp
@@ -1,93 +1,101 @@
 #include <iomanip>
 #include <sstream>
+#include <thread>
 
 #include "chess.hpp"
+#include "threadpool.hpp"
 
 using namespace Chess;
 
-class PerftTest {
-   public:
-    uint64_t nodes;
-    Board board;
+ThreadPool pool(std::thread::hardware_concurrency());
+std::vector<std::future<uint64_t>> futures;
 
-    uint64_t perft(int depth, int max) {
-        Movelist<Move> moves;
-        Movegen::legalmoves<Move, MoveGenType::ALL>(moves, board);
+template <int DEPTH>
+uint64_t perft_thread(Board& board);
 
-        if (depth == 1) {
-            return moves.size();
-        }
+template <int DEPTH>
+uint64_t perft_thread_start(Board board) {
+    return perft_thread<DEPTH>(board);
+}
 
-        U64 nodesIt = 0;
+template <int DEPTH>
+uint64_t perft_thread(Board& board) {
+    Movelist<Move> moves;
+    Movegen::legalmoves<Move, MoveGenType::ALL>(moves, board);
+
+    if constexpr (DEPTH == 1) {
+        return moves.size();
+    } else {
+        uint64_t nodes = 0;
 
         for (int i = 0; i < moves.size(); i++) {
             const auto move = moves[i];
             board.makeMove(move);
-            nodesIt += perft(depth - 1, depth);
-            board.unmakeMove(move);
-            if (depth == max) {
-                nodes += nodesIt;
-                nodesIt = 0;
+
+            if constexpr (DEPTH == 5) {
+                futures.emplace_back(pool.enqueue(perft_thread_start<DEPTH - 1>, board));
+            } else {
+                nodes += perft_thread<DEPTH - 1>(board);
             }
+
+            board.unmakeMove(move);
         }
-
-        return nodesIt;
-    }
-
-    uint64_t testPositionPerft(Board &b, int depth, uint64_t expectedNodeCount) {
-        nodes = 0;
-        std::stringstream ss;
-
-        board = b;
-
-        const auto t1 = std::chrono::high_resolution_clock::now();
-        perft(depth, depth);
-        const auto t2 = std::chrono::high_resolution_clock::now();
-        const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-
-        ss << "depth " << std::left << std::setw(2) << depth << " time " << std::setw(5) << ms
-           << " nodes " << std::setw(12) << nodes << " nps " << std::setw(9)
-           << (nodes * 1000) / (ms + 1) << " fen " << std::setw(87) << board.getFen();
-
-        if (nodes != expectedNodeCount) {
-            ss << "Wrong node count ";
-        }
-
-        std::cout << ss.str() << std::endl;
 
         return nodes;
     }
-};
+}
 
 int main() {
+#ifdef PGO_DEPTH
+    constexpr int kDepth = PGO_DEPTH;
+    Board board = Board();
+#else
+    constexpr int kDepth = 7;
     Board board = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    PerftTest perft = PerftTest();
+#endif  // DEPTH
 
-    U64 totalNodes = 0;
-
+    const auto t0 = std::chrono::high_resolution_clock::now();
+    uint64_t nodes = perft_thread<kDepth>(board);
+    for (auto& future : futures) {
+        nodes += future.get();
+    }
     const auto t1 = std::chrono::high_resolution_clock::now();
-    totalNodes += perft.testPositionPerft(board, 7, 3195901860);
 
-    board.loadFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ");
-    totalNodes += perft.testPositionPerft(board, 5, 193690690);
-
-    board.loadFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ");
-    totalNodes += perft.testPositionPerft(board, 7, 178633661);
-
-    board.loadFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
-    totalNodes += perft.testPositionPerft(board, 6, 706045033);
-
-    board.loadFen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
-    totalNodes += perft.testPositionPerft(board, 5, 89941194);
-
-    board.loadFen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 1");
-    totalNodes += perft.testPositionPerft(board, 5, 164075551);
-
-    const auto t2 = std::chrono::high_resolution_clock::now();
-    const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-
-    std::cout << "\naveraged: \n"
-              << "nodes " << totalNodes << " nps " << (totalNodes * 1000) / (ms + 1) << std::endl;
+    const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
+    std::cout << "nodes " << nodes << " time " << elapsed << " nps "
+              << (nodes * 1000) / (elapsed + 1) << std::endl;
 
     return 0;
 }
+
+// template instantiation
+template uint64_t perft_thread<1>(Board&);
+template uint64_t perft_thread<2>(Board&);
+template uint64_t perft_thread<3>(Board&);
+template uint64_t perft_thread<4>(Board&);
+template uint64_t perft_thread<5>(Board&);
+template uint64_t perft_thread<6>(Board&);
+template uint64_t perft_thread<7>(Board&);
+template uint64_t perft_thread<8>(Board&);
+template uint64_t perft_thread<9>(Board&);
+template uint64_t perft_thread<10>(Board&);
+template uint64_t perft_thread<11>(Board&);
+template uint64_t perft_thread<12>(Board&);
+template uint64_t perft_thread<13>(Board&);
+template uint64_t perft_thread<14>(Board&);
+template uint64_t perft_thread<15>(Board&);
+template uint64_t perft_thread<16>(Board&);
+template uint64_t perft_thread<17>(Board&);
+template uint64_t perft_thread<18>(Board&);
+template uint64_t perft_thread<19>(Board&);
+template uint64_t perft_thread<20>(Board&);
+template uint64_t perft_thread<21>(Board&);
+template uint64_t perft_thread<22>(Board&);
+template uint64_t perft_thread<23>(Board&);
+template uint64_t perft_thread<24>(Board&);
+template uint64_t perft_thread<25>(Board&);
+template uint64_t perft_thread<26>(Board&);
+template uint64_t perft_thread<27>(Board&);
+template uint64_t perft_thread<28>(Board&);
+template uint64_t perft_thread<29>(Board&);
+template uint64_t perft_thread<30>(Board&);
---
+++ src/threadpool.hpp
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <atomic>
+#include <chrono>
+#include <cmath>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <queue>
+#include <stdexcept>
+#include <thread>
+#include <type_traits>
+#include <vector>
+
+class ThreadPool {
+   public:
+    ThreadPool(std::size_t num_threads) : stop_(false) {
+        for (std::size_t i = 0; i < num_threads; ++i)
+            workers_.emplace_back([this] {
+                while (!this->stop_) {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock<std::mutex> lock(this->queue_mutex_);
+                        this->condition_.wait(
+                            lock, [this] { return this->stop_ || !this->tasks_.empty(); });
+                        if (this->stop_ && this->tasks_.empty()) return;
+                        task = std::move(this->tasks_.front());
+                        this->tasks_.pop();
+                    }
+                    task();
+                }
+            });
+    }
+
+    template <class F, class... Args>
+    auto enqueue(F &&f, Args &&...args)
+        -> std::future<typename std::invoke_result<F, Args...>::type> {
+        using return_type = typename std::invoke_result<F, Args...>::type;
+
+        auto task = std::make_shared<std::packaged_task<return_type()>>(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+
+        std::future<return_type> res = task->get_future();
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex_);
+            if (stop_) throw std::runtime_error("Warning: enqueue on stopped ThreadPool");
+            tasks_.emplace([task]() { (*task)(); });
+        }
+        condition_.notify_one();
+        return res;
+    }
+
+    void resize(std::size_t num_threads) {
+        if (num_threads == 0)
+            throw std::invalid_argument("Warning: ThreadPool::resize() - num_threads cannot be 0");
+
+        if (num_threads == workers_.size()) return;
+
+        kill();
+
+        stop_ = false;
+
+        workers_.clear();
+        workers_.resize(num_threads);
+
+        for (std::size_t i = 0; i < num_threads; ++i)
+            workers_.emplace_back([this] {
+                while (!this->stop_) {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock<std::mutex> lock(this->queue_mutex_);
+                        this->condition_.wait(
+                            lock, [this] { return this->stop_ || !this->tasks_.empty(); });
+                        if (this->stop_ && this->tasks_.empty()) return;
+                        task = std::move(this->tasks_.front());
+                        this->tasks_.pop();
+                    }
+                    task();
+                }
+            });
+    }
+
+    void kill() {
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex_);
+            stop_ = true;
+        }
+        condition_.notify_all();
+        for (auto &worker : workers_) {
+            if (worker.joinable()) {
+                worker.join();
+            }
+        }
+
+        workers_.clear();
+    }
+
+    ~ThreadPool() { kill(); }
+
+    std::size_t queueSize() {
+        std::unique_lock<std::mutex> lock(this->queue_mutex_);
+        return tasks_.size();
+    }
+
+    bool getStop() { return stop_; }
+
+   private:
+    std::vector<std::thread> workers_;
+    std::queue<std::function<void()>> tasks_;
+    std::mutex queue_mutex_;
+    std::condition_variable condition_;
+
+    std::atomic_bool stop_;
+};
