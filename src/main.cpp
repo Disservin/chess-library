5591731abb336cd0fde2a717608b33f02da34577
disservin <disservin.social@gmail.com>
2023-06-11 22:18:37 +0200

    refactor: code (#15)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iomanip>
 #include <sstream>
+#include <chrono>
 
 #include "chess.hpp"
 
@@ -11,8 +12,8 @@ class PerftTest {
     Board board;
 
     uint64_t perft(int depth, int max) {
-        Movelist<Move> moves;
-        movegen::legalmoves<Move, MoveGenType::ALL>(moves, board);
+        Movelist moves;
+        movegen::legalmoves(moves, board);
 
         if (depth == 1) {
             return moves.size();
@@ -68,19 +69,19 @@ int main() {
     auto t1 = std::chrono::high_resolution_clock::now();
     totalNodes += perft.testPositionPerft(board, 7, 3195901860);
 
-    board.loadFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ");
+    board.setFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ");
     totalNodes += perft.testPositionPerft(board, 5, 193690690);
 
-    board.loadFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ");
+    board.setFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ");
     totalNodes += perft.testPositionPerft(board, 7, 178633661);
 
-    board.loadFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
+    board.setFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
     totalNodes += perft.testPositionPerft(board, 6, 706045033);
 
-    board.loadFen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
+    board.setFen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
     totalNodes += perft.testPositionPerft(board, 5, 89941194);
 
-    board.loadFen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 1");
+    board.setFen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 1");
     totalNodes += perft.testPositionPerft(board, 5, 164075551);
 
     auto t2 = std::chrono::high_resolution_clock::now();
@@ -91,27 +92,27 @@ int main() {
 
     std::cout << "\nStarting Chess960 tests\n";
 
-    board.setChess960(true);
-    board.loadFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w AHah - 0 1");
+    board.set960(true);
+    board.setFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w AHah - 0 1");
 
     totalNodes = 0;
 
     t1 = std::chrono::high_resolution_clock::now();
     totalNodes += perft.testPositionPerft(board, 6, 119060324ull);
 
-    board.loadFen("1rqbkrbn/1ppppp1p/1n6/p1N3p1/8/2P4P/PP1PPPP1/1RQBKRBN w FBfb - 0 9");
+    board.setFen("1rqbkrbn/1ppppp1p/1n6/p1N3p1/8/2P4P/PP1PPPP1/1RQBKRBN w FBfb - 0 9");
     totalNodes += perft.testPositionPerft(board, 6, 191762235ull);
 
-    board.loadFen("rbbqn1kr/pp2p1pp/6n1/2pp1p2/2P4P/P7/BP1PPPP1/R1BQNNKR w HAha - 0 9");
+    board.setFen("rbbqn1kr/pp2p1pp/6n1/2pp1p2/2P4P/P7/BP1PPPP1/R1BQNNKR w HAha - 0 9");
     totalNodes += perft.testPositionPerft(board, 6, 924181432ull);
 
-    board.loadFen("rqbbknr1/1ppp2pp/p5n1/4pp2/P7/1PP5/1Q1PPPPP/R1BBKNRN w GAga - 0 9");
+    board.setFen("rqbbknr1/1ppp2pp/p5n1/4pp2/P7/1PP5/1Q1PPPPP/R1BBKNRN w GAga - 0 9");
     totalNodes += perft.testPositionPerft(board, 6, 308553169ull);
 
-    board.loadFen("4rrb1/1kp3b1/1p1p4/pP1Pn2p/5p2/1PR2P2/2P1NB1P/2KR1B2 w D - 0 21");
+    board.setFen("4rrb1/1kp3b1/1p1p4/pP1Pn2p/5p2/1PR2P2/2P1NB1P/2KR1B2 w D - 0 21");
     totalNodes += perft.testPositionPerft(board, 6, 872323796ull);
 
-    board.loadFen("1rkr3b/1ppn3p/3pB1n1/6q1/R2P4/4N1P1/1P5P/2KRQ1B1 b Dbd - 0 14");
+    board.setFen("1rkr3b/1ppn3p/3pB1n1/6q1/R2P4/4N1P1/1P5P/2KRQ1B1 b Dbd - 0 14");
     totalNodes += perft.testPositionPerft(board, 6, 2678022813ull);
 
     t2 = std::chrono::high_resolution_clock::now();
