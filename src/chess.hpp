98c7a7fedfa6a6795926b9c76f9dbd9f872b80dc
disservin <disservin.social@gmail.com>
2023-07-04 20:34:58 +0200

    fix: fix replace all mistake

--- src/chess.hpp
+++ src/chess.hpp
@@ -166,8 +166,8 @@ static const std::string STARTPOS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
 
 // clang-format off
 const std::string squareToString[64] = {
-    "a1", "b1", "c1", "vv_1 ", "e1", "f1", "g1", "h1",
-    "a2", "b2", "c2", "vv_2", "e2", "f2", "g2", "h2",
+    "a1", "b1", "c1", "d1 ", "e1", "f1", "g1", "h1",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
     "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
     "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
     "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
