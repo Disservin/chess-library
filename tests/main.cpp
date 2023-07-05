031b6d29d7b631e5e4a0d72f1dec4a2468ad0150
disservin <disservin.social@gmail.com>
2023-07-05 17:00:26 +0200

    refactor: use doctest and test hash generation as well

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,19 @@
-#include "perft.hpp"
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest/doctest.hpp"
 
-int main() {
-    runPerftTest();
-    return 0;
+int main(int argc, char **argv) {
+    doctest::Context ctx;
+
+    ctx.setOption("abort-after", 1);  // default - stop after 5 failed asserts
+    ctx.setOption("no-exitcode", false);
+    ctx.setOption("success", true);
+
+    ctx.applyCommandLine(argc, argv);
+
+    const int res = ctx.run();  // run test cases unless with --no-run
+
+    if (ctx.shouldExit())  // query flags (and --exit) rely on this
+        return res;        // propagate the result of the tests
+
+    return res;
 }
