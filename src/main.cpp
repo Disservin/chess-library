577b71ac1c6dec64a1e1de064488998f70c01ad8
disservin <disservin.social@gmail.com>
2023-07-11 23:13:33 +0200

    feat/fix: pgn functionalities (#20)
    - fix various FRC/DFRC related problems
    - add new pgn parsing functionality (soon to be documented)

--- src/main.cpp
+++ src/main.cpp
@@ -9,5 +9,19 @@ int main() {
 
     std::cout << "board: " << board << std::endl;
 
+    std::ifstream file("64a8ced102cd07745c612a6f-0.pgn");
+
+    while (true) {
+        auto game = pgn::readGame(file);
+
+        if (!game.has_value()) {
+            return 0;
+        }
+
+        if (game.value().moves().size() != std::stoi(game.value().headers().at("PlyCount"))) {
+            std::cout << "Error: moves and result don't match" << std::endl;
+        }
+    }
+
     return 0;
 }
