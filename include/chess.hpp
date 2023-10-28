ba51e6a7a5738d8e9bae21a0cf721079deee59e5
disservin <disservin.social@gmail.com>
2023-10-28 16:50:31 +0200

    constexpr

--- include/chess.hpp
+++ include/chess.hpp
@@ -185,7 +185,7 @@ static const std::string STARTPOS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR
 }  // namespace constants
 
 // clang-format off
-const std::string squareToString[64] = {
+constexpr std::string_view squareToString[64] = {
     "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
     "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
     "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
