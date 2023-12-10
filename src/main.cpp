d31548ac40f6887552e487f66164ea741011e3ea
disservin <disservin.social@gmail.com>
2023-12-10 23:03:56 +0100

    uci

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,6 @@ int main(int argc, char const *argv[]) {
 
     board.setFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
-    std::cout << board << std::endl;
-
     chess::Movelist moves;
     chess::movegen::legalmoves(moves, board);
 
