a6a36b19f382317c581fcbbb8fcd5c33b57eb488
disservin <disservin.social@gmail.com>
2024-03-02 13:45:57 +0100

    fix: uciToMove for frc (#80)

--- src/uci.hpp
+++ src/uci.hpp
@@ -54,9 +54,15 @@ class uci {
         Square target   = Square(uci.substr(2, 2));
         PieceType piece = board.at(source).type();
 
+        // castling in chess960
+        if (board.chess960() && piece == PieceType::KING && board.at(target).type() == PieceType::ROOK &&
+            board.at(target).color() == board.sideToMove()) {
+            return Move::make<Move::CASTLING>(source, target);
+        }
+
         // convert to king captures rook
         // in chess960 the move should be sent as king captures rook already!
-        if (piece == PieceType::KING && Square::distance(target, source) == 2) {
+        if (!board.chess960() && piece == PieceType::KING && Square::distance(target, source) == 2) {
             target = Square(target > source ? File::FILE_H : File::FILE_A, source.rank());
             return Move::make<Move::CASTLING>(source, target);
         }
