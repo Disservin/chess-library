b224161ab797005fcb5928c6177528d57910734e
Disservin <disservin.social@gmail.com>
2025-03-13 11:59:33 +0100

    remove comment

--- fuzz/pgn_fuzzer.cpp
+++ fuzz/pgn_fuzzer.cpp
@@ -22,21 +22,17 @@ class MyVisitor : public pgn::Visitor {
     Board board;
 };
 
-// The fuzzing entry point
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     if (size == 0) return 0;
 
-    // Convert the raw data to a string
     std::string pgn_content(reinterpret_cast<const char*>(data), size);
 
-    // Create a string stream from the data
     std::istringstream input_stream(pgn_content);
 
-    // Create your visitor
     auto vis = std::make_unique<MyVisitor>();
 
     pgn::StreamParser parser(input_stream);
     parser.readGames(*vis);
 
-    return 0;  // Non-zero return values are reserved for future use
+    return 0;
 }
