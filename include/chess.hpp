f7145bed955a87bf8c88d25504e5b10dd1c13324
Disservin <disservin.social@gmail.com>
2025-03-27 19:33:17 +0100

    chore: bump to 0.8.6

--- include/chess.hpp
+++ include/chess.hpp
@@ -25,7 +25,7 @@ THIS FILE IS AUTO GENERATED DO NOT CHANGE MANUALLY.
 
 Source: https://github.com/Disservin/chess-library
 
-VERSION: 0.8.5
+VERSION: 0.8.6
 */
 
 #ifndef CHESS_HPP
@@ -66,17 +66,12 @@ class Color {
     enum class underlying : std::int8_t { WHITE = 0, BLACK = 1, NONE = -1 };
 
     constexpr Color() : color(underlying::NONE) {}
-    constexpr Color(underlying c) : color(c) {
-        assert(c == underlying::WHITE || c == underlying::BLACK || c == underlying::NONE);
-    }
-    constexpr Color(int c) : color(static_cast<underlying>(c)) { assert(c == 0 || c == 1 || c == -1); }
-    constexpr Color(std::string_view str) : color(underlying::NONE) {
-        if (str == "w") {
-            color = underlying::WHITE;
-        } else if (str == "b") {
-            color = underlying::BLACK;
-        }
-    }
+    constexpr Color(underlying c) : color(c) { assert(isValid(int(c))); }
+    constexpr Color(int c) : Color(static_cast<underlying>(c)) { assert(isValid(c)); }
+    constexpr Color(std::string_view str)
+        : color(str == "w"   ? underlying::WHITE
+                : str == "b" ? underlying::BLACK
+                             : underlying::NONE) {}
 
     /**
      * @brief Gets the long string representation of the color
@@ -94,17 +89,9 @@ class Color {
         }
     }
 
-    constexpr Color operator~() const noexcept { return static_cast<Color>(static_cast<uint8_t>(color) ^ 1); }
-
-    explicit operator std::string() const {
-        switch (color) {
-            case underlying::WHITE:
-                return "w";
-            case underlying::BLACK:
-                return "b";
-            default:
-                return "NONE";
-        }
+    constexpr Color operator~() const noexcept {
+        assert(color != underlying::NONE);
+        return Color(static_cast<underlying>(static_cast<int>(color) ^ 1));
     }
 
     constexpr bool operator==(const Color& rhs) const noexcept { return color == rhs.color; }
@@ -112,9 +99,15 @@ class Color {
 
     constexpr operator int() const noexcept { return static_cast<int>(color); }
 
+    explicit operator std::string() const {
+        return color == underlying::WHITE ? "w" : color == underlying::BLACK ? "b" : "NONE";
+    }
+
     [[nodiscard]] constexpr underlying internal() const noexcept { return color; }
 
-    friend std::ostream& operator<<(std::ostream& os, const Color& color);
+    friend std::ostream& operator<<(std::ostream& os, const Color& color) {
+        return os << static_cast<std::string>(color);
+    }
 
     static constexpr underlying WHITE = underlying::WHITE;
     static constexpr underlying BLACK = underlying::BLACK;
@@ -122,19 +115,14 @@ class Color {
 
    private:
     underlying color;
-};  // namespace chess
 
-inline std::ostream& operator<<(std::ostream& os, const Color& color) { return os << static_cast<std::string>(color); }
+    static constexpr bool isValid(int c) { return c == 0 || c == 1 || c == -1; }
+};
 
 constexpr Color::underlying operator~(Color::underlying color) {
-    switch (color) {
-        case Color::underlying::WHITE:
-            return Color::underlying::BLACK;
-        case Color::underlying::BLACK:
-            return Color::underlying::WHITE;
-        default:
-            return Color::underlying::NONE;
-    }
+    return color == Color::underlying::WHITE   ? Color::underlying::BLACK
+           : color == Color::underlying::BLACK ? Color::underlying::WHITE
+                                               : Color::underlying::NONE;
 }
 
 }  // namespace chess
