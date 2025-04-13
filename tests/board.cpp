918278a3ea42771f721e9bed1b7f0d94ad340646
Disservin <disservin.social@gmail.com>
2025-04-13 15:07:39 +0200

    fixes

--- tests/board.cpp
+++ tests/board.cpp
@@ -6,6 +6,12 @@
 using namespace chess;
 
 TEST_SUITE("Board") {
+    TEST_CASE("Set Fen") {
+        Board board;
+        CHECK(board.setFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - "));
+        CHECK(board.getFen() == "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+    }
+
     TEST_CASE("Board makeMove/unmakeMove") {
         SUBCASE("makeMove") {
             Board board = Board();
